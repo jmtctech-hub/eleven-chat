cbe071f13c598cdc806e5e969b298a30c6a1bb85
jmtctech-hub <jmtctech@gmail.com>
2026-05-15 22:55:47 +0800

    Add start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ELEVEN Chat Launcher (WSL)
+echo "Starting ELEVEN Chat Server..."
+echo "Open http://localhost:8899 in your Windows browser"
+echo ""
+cd "/mnt/d/hermes project/eleven-chat"
+python3 server.py
